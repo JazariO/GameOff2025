74c7e064784f0bbf4bc317d858bba0890121d5b8
JazariO <djazz5112@gmail.com>
2025-11-24 08:53:06 +1100

    change(PhotographicCameraController): basic photo camera movement with input

--- Assets/Scripts/PhotographicCameraSystem/PhotographicCameraController.cs
+++ Assets/Scripts/PhotographicCameraSystem/PhotographicCameraController.cs
@@ -1,16 +1,62 @@
+using Proselyte.Sigils;
+using System;
 using UnityEngine;
 
 public class PhotographicCameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] PlayerInputDataSO playerInputDataSO;
+
+    [SerializeField] GameEvent OnInspectPhotographicCameraEngage;
+    [SerializeField] GameEvent OnInspectPhotographicCameraDisengage;
+
+    [SerializeField] Transform photographicCameraPivotTransform;
+
+    [Serializable] struct CameraControls
     {
-        
+        public float rotation_limit_pitch_min;
+        public float rotation_limit_pitch_max;
+        public float rotation_limit_yaw_min;
+        public float rotation_limit_yaw_max;
+        public float zoom_fov_limit_min;
+        public float zoom_fov_limit_max;
+        public float zoom_fov_speed;
+    } [SerializeField] CameraControls cameraControls;
+
+    private bool engaged;
+    private bool is_zooming;
+    
+    private float pitch;
+    private float yaw;
+    private float zoom;
+
+    private void OnEnable()
+    {
+        OnInspectPhotographicCameraEngage.RegisterListener(HandleInspectEngage);
+        OnInspectPhotographicCameraDisengage.RegisterListener(HandleInspectDisengage);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        OnInspectPhotographicCameraEngage.UnregisterListener(HandleInspectEngage);
+        OnInspectPhotographicCameraDisengage.UnregisterListener(HandleInspectDisengage);
     }
+
+    private void Update()
+    {
+        if(engaged)
+        {
+            // Rotate photographic camera using player move input
+            // Accumulate player input
+            pitch -= playerInputDataSO.input_move.y;
+            yaw += playerInputDataSO.input_move.x;
+
+            pitch = Mathf.Clamp(pitch, cameraControls.rotation_limit_pitch_min, cameraControls.rotation_limit_pitch_max);
+            yaw = Mathf.Clamp(yaw, cameraControls.rotation_limit_yaw_min, cameraControls.rotation_limit_yaw_max);
+
+            photographicCameraPivotTransform.localRotation = Quaternion.Euler(pitch, yaw, 0);
+        }
+    }
+
+    public void HandleInspectEngage() { engaged = true; }
+    public void HandleInspectDisengage() { engaged = false; }
 }
