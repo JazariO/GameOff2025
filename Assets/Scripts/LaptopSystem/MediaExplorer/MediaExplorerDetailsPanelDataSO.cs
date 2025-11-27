05d511226ec0406f778b5ba347c84939f550006e
JazariO <djazz5112@gmail.com>
2025-11-27 14:21:50 +1100

    add(MediaExplorerDetailsPanelDataSO)

---
+++ Assets/Scripts/LaptopSystem/MediaExplorer/MediaExplorerDetailsPanelDataSO.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MediaExplorerDetailsPanelDataSO", menuName = "Media Explorer / Details Panel Data SO")]
+public class MediaExplorerDetailsPanelDataSO : ScriptableObject
+{
+    public CrunchOSFile.CrunchFileData crunch_OS_file_data;
+}
